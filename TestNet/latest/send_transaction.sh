fae9a09d766cc74c96a115ed424609ee40dbbf86
Trevor Knott <tknott95@hotmail.com>
2022-02-15 19:22:32 -0700

    new.shell.script.send.transactions.sh

--- TestNet/latest/send_transaction.sh
+++ TestNet/latest/send_transaction.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 curl --request POST \
-  --url http://localhost:1338/v2/wallets/$1/transactions \
+  --url http://localhost:1338/v2/wallets/b3dacef55214a547bf7623beb53783792fd4f329/transactions \
   --header 'Content-Type: application/json' \
   --data '{
-    "passphrase": "test123456",
+    "passphrase": "KennyPowers",
     "payments": [
         {
             "address": "addr_test1qrxwfc90q28w3gs3vtqgadtzsa3slevfrua9qv95qwwst0tafnsvznh7g5yr25u8jk3h3xyd2rff25phwz4yqwd6zazqs0grg0",
@@ -14,4 +14,26 @@ curl --request POST \
             }
         }
     ],
-}'
+    "metadata": {
+        "1337": {
+            "map": [
+                {
+                    "k": {
+                        "string": "test"
+                    },
+                    "v": {
+                        "string": "hello world"
+                    }
+                },
+                {
+                    "k": {
+                        "string": "completed"
+                    },
+                    "v": {
+                        "int": 0
+                    }
+                }
+            ]
+        }
+    }
+  }'
