145350c1bbee78cd97bb9f5ac0e89ad9b527fdbf
Trevor Knott <tknott95@hotmail.com>
2022-07-09 09:20:04 -0600

    shell scripts to setup a tooling build out  | building_trans.sh

--- TestNet/VasilFork/smart_contracts/scripts/build_trans.sh
+++ TestNet/VasilFork/smart_contracts/scripts/build_trans.sh
@@ -12,16 +12,7 @@ utxo_consuming=4ca42623c58dd0efc52790ab20c200a67ed3f70f3e273b800884093a0ea83678#
 x4Wallet137_addr=addr_test1qpdvvdalsqscc3899gk67zdx7lkrlqlnwm3xzzk88jc65c50a06ns46p0wjxe6xqkvnrs4f79wjp6tz07wrl2k2nctyqqkhtak
 paying_to_addr=$x4Wallet137_addr
 
-out_file_loc=../transactions/tx_00/tx.build
-
-# cardano-cli transaction build \
-#   --babbage-era \
-#   $MAGIC \
-#   --change-address $pay_addr_00 \  
-#   --tx-in $utxo_consuming \
-#   --tx-out $payingto_addr+100000000 \
-#   --out-file $out_file_loc
-
+out_file_loc=../transactions/tx00/tx.build
 
 
 echo -e "\e[0;1;30m  UTXO CONSUMED: $utxo_consuming \e[0m"
