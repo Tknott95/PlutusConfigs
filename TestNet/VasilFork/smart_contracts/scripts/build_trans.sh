e5bb4c2a144a412db3f16a76a6ee6c9e5ba6809e
Trevor Knott <tknott95@hotmail.com>
2022-07-09 09:19:33 -0600

    shell scripts to setup a tooling build out  | building_trans.sh

--- TestNet/VasilFork/smart_contracts/scripts/build_trans.sh
+++ TestNet/VasilFork/smart_contracts/scripts/build_trans.sh
@@ -35,7 +35,7 @@ cardano-cli transaction build \
   --change-address $pay_addr_00 \
   --tx-in $utxo_consuming \
   --tx-out $paying_to_addr+10000000 \
-  --out-file ../transactions/tx.build
+  --out-file $out_file_loc
 
 # EXAMPLE
 # cardano-cli transaction build --babbage-era $MAGIC --change-address $(cat ../keys/alwayssucceeds/alwayssucceeds_payment.addr) --tx-in 4ca42623c58dd0efc52790ab20c200a67ed3f70f3e273b800884093a0ea83678#0 --tx-out addr_test1qpdvvdalsqscc3899gk67zdx7lkrlqlnwm3xzzk88jc65c50a06ns46p0wjxe6xqkvnrs4f79wjp6tz07wrl2k2nctyqqkhtak+10000000 --out-file tx.build
