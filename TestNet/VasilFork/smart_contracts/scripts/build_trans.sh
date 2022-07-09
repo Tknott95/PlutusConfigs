f13e68578154ff54a86234c5b2e837674e759acb
Trevor Knott <tknott95@hotmail.com>
2022-07-09 09:32:30 -0600

    shell scripts to setup a tooling build out  | building_trans.sh

--- TestNet/VasilFork/smart_contracts/scripts/build_trans.sh
+++ TestNet/VasilFork/smart_contracts/scripts/build_trans.sh
@@ -17,12 +17,9 @@
 #read $input_piped_in
 # (cat ../keys/alwayssucceeds/alwayssucceeds_payment.addr)
 pay_addr_00=$1
-# addr_test1qrpxufgw8y6dgyl758s37fcea2gm0pvfyrwnths06utp9tr2fgmkqt63xvatw9uufc4q9sdfrwt4hzmp54v6s9jlv2aq0ptj4v
-# $input_piped_in
-# make utxo consuming dynamic with another script
+
 echo "$2"
 utxo_consuming=$2
-# 4ca42623c58dd0efc52790ab20c200a67ed3f70f3e273b800884093a0ea83678#0
 
 x4Wallet137_addr=addr_test1qpdvvdalsqscc3899gk67zdx7lkrlqlnwm3xzzk88jc65c50a06ns46p0wjxe6xqkvnrs4f79wjp6tz07wrl2k2nctyqqkhtak
 paying_to_addr=$x4Wallet137_addr
