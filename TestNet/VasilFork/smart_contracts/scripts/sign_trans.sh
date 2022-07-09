2dd4200d01f259a0b2c35437e36e4451de5d167b
Trevor Knott <tknott95@hotmail.com>
2022-07-09 09:51:36 -0600

    shell scripts to setup a tooling build out  | sign_trans.sh

--- TestNet/VasilFork/smart_contracts/scripts/sign_trans.sh
+++ TestNet/VasilFork/smart_contracts/scripts/sign_trans.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+# ./sign_trans.sh ../transactions/tx00/tx.build  ../keys/alwayssucceeds/alwayssucceeds_payment.skey  ../transactions/tx00/tx.signed
+
+# ./sign_trans.sh \
+# ../transactions/tx00/tx.build \
+#  ../keys/alwayssucceeds/alwayssucceeds_payment.skey \
+#  ../transactions/tx00/tx.signed
+
 built_trans=$1
 
-# payment.skey
+# ../keys/alwayssucceeds/alwayssucceeds_payment.skey
 signing_key_file=$2
 
-signed_out_file=../transactions/tx00/tx.signed
+# ../transactions/tx00/tx.signed
+signed_out_file=$3
 
 echo -e "\e[0;1;30m  BUILT TRANS: $built_trans \e[0m"
 echo -e "\e[0;0;33m  SIGNING KEY: $signing_key_file \e[0m"
-echo -e "\e[0;0;36m  SINGED TRANS AT: $out_file_loc \e[0m"
+echo -e "\e[0;0;36m  SINGED TRANS AT: $signed_out_file \e[0m"
 
 cardano-cli transaction sign \
 --tx-body-file $built_trans \
