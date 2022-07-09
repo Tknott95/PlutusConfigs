fe992c997618e50f49632671b225bfd325138ab8
Trevor Knott <tknott95@hotmail.com>
2022-07-09 09:24:08 -0600

    shell scripts to setup a tooling build out  | building_trans.sh

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 
 
-
 cat ../keys/alwayssucceeds/alwayssucceeds_payment.addr | ./query_head.sh
 
-cat ../keys/alwayssucceeds/alwayssucceeds_payment.addr | ./build_trans.sh 
+
+``` Build Trans
+ ./build_trans.sh $(cat ../keys/alwayssucceeds/alwayssucceeds_payment.addr)
+```
+
+
+DEPRECATED: cat ../keys/alwayssucceeds/alwayssucceeds_payment.addr | ./build_trans.sh 
--- TestNet/VasilFork/smart_contracts/scripts/build_trans.sh
+++ TestNet/VasilFork/smart_contracts/scripts/build_trans.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 
-#read $input_piped_in
+#RUNNING:  ./build_trans.sh $(cat ../keys/alwayssucceeds/alwayssucceeds_payment.addr)
 
+# EXAMPLE
+# cardano-cli transaction build --babbage-era $MAGIC --change-address $(cat ../keys/alwayssucceeds/alwayssucceeds_payment.addr) --tx-in 4ca42623c58dd0efc52790ab20c200a67ed3f70f3e273b800884093a0ea83678#0 --tx-out addr_test1qpdvvdalsqscc3899gk67zdx7lkrlqlnwm3xzzk88jc65c50a06ns46p0wjxe6xqkvnrs4f79wjp6tz07wrl2k2nctyqqkhtak+10000000 --out-file tx.build
+
+
+
+# piping in causes a issues so I am using args here
+#read $input_piped_in
 # (cat ../keys/alwayssucceeds/alwayssucceeds_payment.addr)
-pay_addr_00=addr_test1qrpxufgw8y6dgyl758s37fcea2gm0pvfyrwnths06utp9tr2fgmkqt63xvatw9uufc4q9sdfrwt4hzmp54v6s9jlv2aq0ptj4v
+pay_addr_00=$1
+# addr_test1qrpxufgw8y6dgyl758s37fcea2gm0pvfyrwnths06utp9tr2fgmkqt63xvatw9uufc4q9sdfrwt4hzmp54v6s9jlv2aq0ptj4v
 # $input_piped_in
 # make utxo consuming dynamic with another script
 utxo_consuming=4ca42623c58dd0efc52790ab20c200a67ed3f70f3e273b800884093a0ea83678#0
@@ -28,5 +36,3 @@ cardano-cli transaction build \
   --tx-out $paying_to_addr+10000000 \
   --out-file $out_file_loc
 
-# EXAMPLE
-# cardano-cli transaction build --babbage-era $MAGIC --change-address $(cat ../keys/alwayssucceeds/alwayssucceeds_payment.addr) --tx-in 4ca42623c58dd0efc52790ab20c200a67ed3f70f3e273b800884093a0ea83678#0 --tx-out addr_test1qpdvvdalsqscc3899gk67zdx7lkrlqlnwm3xzzk88jc65c50a06ns46p0wjxe6xqkvnrs4f79wjp6tz07wrl2k2nctyqqkhtak+10000000 --out-file tx.build
