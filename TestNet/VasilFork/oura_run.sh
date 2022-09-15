d1b37f4dfe8502d3a247812342c5a6855f03adf7
tknott95 <tknott95@hotmail.com>
2022-09-14 22:03:52 -0600

    oura_run.sh | watch_head.sh

---
+++ TestNet/VasilFork/oura_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+: '
+  OURA - using it to watch the testnet and most likely filter for testing smart contracts and such
+  
+  DOCS: https://txpipe.github.io/oura/introduction.html
+
+  Can be used to filter if needed with a db - this could allow for fun things to happen
+'
+
+oura watch socket/node.socket --bearer unix --magic testnet
---
+++ TestNet/VasilFork/watch_head.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+watch -n 1 cardano-cli -- query tip $MAGIC 
+
+# --testnet-magic 1097911063
