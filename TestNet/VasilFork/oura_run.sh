d10f4ba0c9b290ba723b8bfde86fbce74abf894d
tknott95 <tknott95@hotmail.com>
2022-09-16 00:02:42 -0600

    oura daemon

--- TestNet/VasilFork/oura_run.sh
+++ TestNet/VasilFork/oura_run.sh
@@ -5,9 +5,14 @@
   
   DOCS: https://txpipe.github.io/oura/introduction.html
 
+
   Can be used to filter if needed with a db - this could allow for fun things to happen
+
+ Sink to terminal to simply "watch" with filters and such
+
+ - This is where you connect into a cb (trough the config)
+
 '
 
-oura watch socket/node.socket --bearer unix --magic testnet 
-
-# --config oura.config
+# oura watch socket/node.socket --bearer unix --magic testnet 
+oura daemon --config oura_config.toml
