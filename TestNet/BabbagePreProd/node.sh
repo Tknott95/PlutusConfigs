88ea0cd701eaf4a33c87c93c1bbf6406724c010a
tknott95 <tknott95@hotmail.com>
2022-12-15 05:12:00 -0700

    BabbagePreProd init

--- TestNet/BabbagePreProd/node.sh
+++ TestNet/BabbagePreProd/node.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cardano-node run +RTS -M3G -RTS --config testnet-config.json --socket-path socket/node.socket  --database-path db/chain-index.db --host-addr 127.0.0.1 --port 1337  --topology testnet-topology.json  #  > node.log 
+cardano-node run +RTS -M3G -RTS --config config.json --socket-path socket/node.socket  --database-path db/chain-index.db --host-addr 127.0.0.1 --port 1337  --topology topology.json  #  > node.log 
 
