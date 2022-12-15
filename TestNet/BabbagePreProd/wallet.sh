e370ddbef5e8c7b5be83ae0273fc6885714fea15
tknott95 <tknott95@hotmail.com>
2022-12-15 05:50:08 -0700

    wallet.sh

--- TestNet/BabbagePreProd/wallet.sh
+++ TestNet/BabbagePreProd/wallet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cardano-wallet serve --testnet testnet-byron-genesis.json  --port 1338 --node-socket socket/node.socket
+cardano-wallet serve --testnet byron-genesis.json  --port 1338 --node-socket socket/node.socket
