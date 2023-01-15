88aab02cc6ec0e7486e1acfc9f723980e5a446cd
tknott95 <tknott95@hotmail.com>
2023-01-15 02:55:03 -0700

    wallet.sh pub ip

--- TestNet/BabbagePreview/wallet.sh
+++ TestNet/BabbagePreview/wallet.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-cardano-wallet serve --testnet byron-genesis.json  --port 1338 --node-socket socket/node.socket
+isLocal=$1
+localIP=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+if [ $isLocal == "pub" ]; then
+  cardano-wallet serve --listen-address 192.168.0.8 --testnet byron-genesis.json  --port 1338 --node-socket socket/node.socket
+else 
+  cardano-wallet serve --testnet byron-genesis.json  --port 1338 --node-socket socket/node.socket
+fi
